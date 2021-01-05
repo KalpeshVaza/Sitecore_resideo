91fe671532e7be6660e7ba55dfb2a2aea195f122
bharath <Bharath.k@biztechnosys.com>
2021-01-05 12:01:04 +0000

    moved some code to repo

--- Resideo/Controllers/NavigationController.cs
+++ Resideo/Controllers/NavigationController.cs
@@ -13,13 +13,6 @@ namespace Sitecore.Resideo.Controllers
 {
     public class NavigationController : SitecoreController
     {
-        //private readonly IProductRepository _productsRepository;
-
-        //public NavigationController(IProductRepository productRepository)
-        //{
-        //    _productsRepository = productRepository;
-        //}
-        // GET: Navigation
         public ActionResult GetNavigation()
         {
             var user = Context.User;
@@ -30,26 +23,8 @@ namespace Sitecore.Resideo.Controllers
         {
             try
             {
-                var homePage = Sitecore.Context.Database.GetItem(Constants.SiteHomePage);
-                var items = homePage?.GetChildren().Where(w => w?.TemplateID == Constants.ProductsTemplateId)?.ToList();
-                List<Product> products = new List<Product>();
-                if (items != null && items.Count > 0)
-                {
-                    foreach (var item in items)
-                    {
-                        var linkField = (LinkField)item.Fields[Templates.Product.Fields.ProductLink];
-                        var imageField = (ImageField)item.Fields[Templates.Product.Fields.ProductImage];
-                        Product product = new Product()
-                        {
-                            ProductTitle = item.Fields[Templates.Product.Fields.ProductTitle].Value,
-                            ProductDescription = item.Fields[Templates.Product.Fields.ProductDescription].Value,
-                            ProductImageUrl = MediaManager.GetMediaUrl(imageField?.MediaItem, new MediaUrlOptions { AlwaysIncludeServerUrl = true }),
-                            ProductLink = linkField?.Url,
-                            ProductLinkText = linkField?.Text
-                        };
-                        products.Add(product);
-                    }
-                }
+                ProductRepository productRepository = new ProductRepository();
+                List<Product> products = productRepository.GetProducts();
                 return View("~/Views/Navigation/Products.cshtml", products);
             }
             catch (Exception ex)
@@ -57,16 +32,6 @@ namespace Sitecore.Resideo.Controllers
                 Log.Error($"{typeof(ProductRepository)}::{nameof(GetProducts)}", ex, Context.User);
                 return View("~/Views/Navigation/Products.cshtml", new List<Product>());
             }
-            //try
-            //{
-            //    var products = _productsRepository.GetProducts();
-            //    return View("~/Views/Navigation/Products.cshtml", products);
-            //}
-            //catch (Exception ex)
-            //{
-            //    Log.Error($"{typeof(ProductsController)}::{nameof(GetProducts)}", ex, Context.User);
-            //    return View("~/Views/Navigation/Products.cshtml", new List<Product>());
-            //}
         }
     }
 } 
